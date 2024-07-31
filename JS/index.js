4e4eb557359f634d518ad4f66ae37173da846a6e
OlaiwonAbdullahi <132976400+OlaiwonAbdullahi@users.noreply.github.com>
2024-07-31 12:03:10 +0100

    new

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,5 @@
-const myH2 = getElementById("myH2")
+//const myH2 = getElementById("myH2")
 
-let username = window.prompt("what's your username?")
+//let username = window.prompt("what's your username?")
 
- document.getElementById("myH2").textContent = `Hello ${username}`
+ //document.getElementById("myH2").textContent = `Hello ${username}`
